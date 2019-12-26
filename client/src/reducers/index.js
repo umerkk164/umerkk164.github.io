ca72c3f240941e1a6f12869645d19baae6754db7
Umer Khan <umerkk164@gmail.com>
2019-12-26 22:02:06 +0500

    aa

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import boardReducer from './boardReducer';
-import authReducer from './authReducer';
-
-export default combineReducers({
-    board: boardReducer,
-    auth: authReducer
-});
